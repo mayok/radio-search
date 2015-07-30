4c7a91b58496d5e525d794c73855ff096c5fa71b
Naohiro IWAMOTO <mayok@club.kyutech.ac.jp>
2015-07-30 15:57:48 +0900

    improve test

--- test/controllers/channels_controller_test.rb
+++ test/controllers/channels_controller_test.rb
@@ -1,10 +1,15 @@
 require 'test_helper'
 
 class ChannelsControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Internet Radio Programs"
+  end
+
   test "should get search" do
     get :search
     assert_response :success
-    assert_select "title", "Search | Internet Radio Programs"
+    assert_select "title", "Search | #{@base_title}"
   end
 
 end
