a69d4d6bc579435ee22d8c5936bccdb486d0c166
Naohiro IWAMOTO <mayok@club.kyutech.ac.jp>
2015-07-30 15:51:09 +0900

    improve search view
    - add input, button

--- test/controllers/channels_controller_test.rb
+++ test/controllers/channels_controller_test.rb
@@ -4,6 +4,7 @@ class ChannelsControllerTest < ActionController::TestCase
   test "should get search" do
     get :search
     assert_response :success
+    assert_select "title", "Search | Internet Radio Programs"
   end
 
 end
